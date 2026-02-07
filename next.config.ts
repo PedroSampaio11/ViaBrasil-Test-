12b7b6851738b940eedf527b13ceba1f2b1ad7d8
Wallace-Bezerra <wallace_2014_@hotmail.com>
2026-02-07 10:58:20 -0300

    Update Next.js configuration to allow images from autocerto.com and add new dependencies for Radix UI components including popover and select. Enhance package-lock.json with additional packages for improved functionality.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "autocerto.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "autocerto.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
